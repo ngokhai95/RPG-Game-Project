054509ab59e6d6231b371c439505beac2ac8af12
Khai <ngokhai1995@gmail.com>
2020-10-08 00:45:30 -0700

    Added Tree/Enemy Spawner Agent

---
+++ Scripts/SpawnerAgent.cs
@@ -0,0 +1,237 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class SpawnerAgent : Agent
+{
+    float killer;
+    float location;
+    int treeID;
+    EnemySpawner enemySpawner;
+    TreeSpawner treeSpawner;
+
+    public override void Initialize()
+    {
+        RandomParameter();
+        enemySpawner = GetComponent<EnemySpawner>();
+        treeSpawner = GetComponent<TreeSpawner>();
+        treeID = -1;
+        MaxStep = 1000;
+    }
+    private void Update()
+    {
+        EnemyRewardFunction();
+        TreeRewardFunction();
+        Debug.Log(GetCumulativeReward());
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(killer);
+        sensor.AddObservation(enemySpawner.numObjects);
+        sensor.AddObservation(location);
+        sensor.AddObservation(treeID);
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        treeID = -1;
+        RandomParameter();
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        if (Mathf.FloorToInt(vectorAction[0]) == 0) 
+        {
+            enemySpawner.numObjects = 0;
+        }
+        if (Mathf.FloorToInt(vectorAction[0]) == 1) 
+        {
+            enemySpawner.numObjects = 1;
+        }
+        if (Mathf.FloorToInt(vectorAction[0]) == 2) 
+        {
+            enemySpawner.numObjects = 2;
+        }
+        if (Mathf.FloorToInt(vectorAction[0]) == 3) 
+        {
+            enemySpawner.numObjects = 3;
+        }
+        if (Mathf.FloorToInt(vectorAction[0]) == 4) 
+        {
+            enemySpawner.numObjects = 4;
+        }
+        if (Mathf.FloorToInt(vectorAction[0]) == 5)
+        {
+            enemySpawner.numObjects = 5;
+        }
+        if (Mathf.FloorToInt(vectorAction[0]) == 6)
+        {
+            enemySpawner.numObjects = 6;
+        }
+        if (Mathf.FloorToInt(vectorAction[0]) == 7)
+        {
+            enemySpawner.numObjects = 7;
+        }
+        if (Mathf.FloorToInt(vectorAction[0]) == 8)
+        {
+            enemySpawner.numObjects = 8;
+        }
+        if (Mathf.FloorToInt(vectorAction[0]) == 9)
+        {
+            enemySpawner.numObjects = 9;
+        }
+        if (Mathf.FloorToInt(vectorAction[0]) == 10)
+        {
+            enemySpawner.numObjects = 10;
+        }
+
+        if (Mathf.FloorToInt(vectorAction[1]) == 0)
+        {
+            treeSpawner.prefab = GameObject.FindWithTag("Mountain Tree");
+            treeID = 0;
+        }
+        if (Mathf.FloorToInt(vectorAction[1]) == 1)
+        {
+            treeSpawner.prefab = GameObject.FindWithTag("Forest Tree");
+            treeID = 1;
+        }
+        if (Mathf.FloorToInt(vectorAction[1]) == 2)
+        {
+            treeSpawner.prefab = GameObject.FindWithTag("Desert Tree");
+            treeID = 2;
+        }
+        if (Mathf.FloorToInt(vectorAction[1]) == 3)
+        {
+            treeSpawner.prefab = GameObject.FindWithTag("River Tree");
+            treeID = 3;
+        }
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+            actionsOut[0] = 0;
+            actionsOut[0] = 1;
+            actionsOut[0] = 2;
+            actionsOut[0] = 3;
+            actionsOut[0] = 4;
+            actionsOut[0] = 5;
+            actionsOut[0] = 6;
+            actionsOut[0] = 7;
+            actionsOut[0] = 8;
+            actionsOut[0] = 9;
+            actionsOut[0] = 10;
+
+            actionsOut[1] = 0;
+            actionsOut[1] = 1;
+            actionsOut[1] = 2;
+            actionsOut[1] = 3;
+
+    }
+
+    private void EnemyRewardFunction()
+    {
+        if(killer <= 3)
+        {
+            if(enemySpawner.numObjects > 3)
+            {
+                AddReward(-1.0f / MaxStep);
+            }
+            else
+            {
+                AddReward(1.0f / MaxStep);
+            }
+        }
+        else if (killer <= 5)
+        {
+            if (enemySpawner.numObjects > 5)
+            {
+                AddReward(-1.0f / MaxStep);
+            }
+            else
+            {
+                if (enemySpawner.numObjects > 3)
+                    AddReward(1.0f / MaxStep);
+                else 
+                    AddReward(0.5f / MaxStep);
+            }
+        }
+        else if (killer <= 8)
+        {
+            if (enemySpawner.numObjects > 8)
+            {
+                AddReward(-1.0f / MaxStep);
+            }
+            else
+            {
+                if (enemySpawner.numObjects > 5)
+                    AddReward(1.0f / MaxStep);
+                else if (enemySpawner.numObjects > 3)
+                    AddReward(0.5f / MaxStep);
+                else
+                {
+                    AddReward(0.25f / MaxStep);
+                }
+            }
+        }
+        else
+        {
+            if (enemySpawner.numObjects > 10)
+            {
+                AddReward(-1.0f / MaxStep);
+            }
+            else
+            {
+                if (enemySpawner.numObjects > 8)
+                    AddReward(1.0f / MaxStep);
+                else if (enemySpawner.numObjects > 5)
+                    AddReward(0.5f / MaxStep);
+                else if (enemySpawner.numObjects > 3)
+                    AddReward(0.25f / MaxStep);
+                else
+                {
+                    AddReward(0.125f / MaxStep);
+                }
+            }
+        }
+    }
+
+    private void TreeRewardFunction()
+    {
+        if (location == treeID)
+        {
+            AddReward(1.0f / MaxStep);
+        }
+        else
+        {
+            AddReward(-1.0f / MaxStep);
+        }
+    }
+
+    private void RandomParameter()
+    {
+        killer = Random.Range(0.0f, 10.0f);
+        Debug.Log(killer);
+        location = Random.Range(0, 3);
+        switch (location)
+        {
+            case 0:
+                Debug.Log("Mountain");
+                break;
+            case 1:
+                Debug.Log("Forest");
+                break;
+            case 2:
+                Debug.Log("Desert");
+                break;
+            case 3:
+                Debug.Log("River");
+                break;
+        }
+    }
+
+}
+
+
---
+++ Scripts/TreeSpawner.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEditor;
+
+public class TreeSpawner : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject prefab;
+    public int numObjects;
+    public float spawnSize;
+    private LayerMask layerMask = ~0;
+    private int seedNo = 12345;
+    private Vector3 offset;
+
+
+    private float checkingHeight = 100000f;
+    private Quaternion rotation;
+    
+    [HideInInspector] public GameObject[] spawnObjs;
+    private GameObject spawnObj;
+    private bool updateSettings = false;
+
+    private void OnValidate()
+    {
+        if (Time.realtimeSinceStartup > 10f)
+            updateSettings = true;
+
+#if UNITY_EDITOR
+        UnityEditor.SceneView.RepaintAll();
+#endif
+    }
+
+    private void OnEnable()
+    {
+        if (Time.realtimeSinceStartup > 10f)
+            InitObjects();
+    }
+
+    private void OnDisable()
+    {
+        RemoveObjects();
+    }
+
+    private void CreateParent()
+    {
+        if (GameObject.Find("Tree Spawn") == null)
+            spawnObj = new GameObject("Tree Spawn");
+        else
+            spawnObj = GameObject.Find("Tree Spawn");
+    }
+
+    private void RemoveParent()
+    {
+        if (GameObject.Find("Tree Spawn") != null)
+            DestroyImmediate(GameObject.Find("Tree Spawn"));
+    }
+
+    private void RemoveObjects()
+    {
+        if (spawnObjs != null && spawnObjs.Length > 0)
+            for (int i = 0; i < spawnObjs.Length; i++)
+                DestroyImmediate(spawnObjs[i]);
+    }
+
+    private void Awake()
+    {
+        if (Application.isPlaying)
+            RemoveParent();
+    }
+
+    private void Start()
+    {
+        if (Application.isPlaying)
+            InitObjects();
+    }
+
+    void Update()
+    {
+        if (player == null || prefab == null) return;
+        UpdateObjects();
+    }
+
+    void InitObjects()
+    {
+        spawnObjs = new GameObject[numObjects];
+        if (player == null || prefab == null) return;
+        CreateParent();
+        CreateObjects();
+    }
+
+    private void CreateObjects()
+    {
+        for (int i = 0; i < numObjects; i++)
+        {
+            spawnObjs[i] = Instantiate(prefab, spawnObj.transform);
+            spawnObjs[i].name = prefab.name + "_" + (i + 1).ToString();
+        }
+    }
+
+    private void Spawn(int i, Vector3 origin)
+    {
+        if (player == null || prefab == null) return;
+
+        Physics.autoSimulation = false;
+        Physics.Simulate(Time.fixedDeltaTime);
+        origin.y = checkingHeight;
+        Ray ray = new Ray(origin, Vector3.down);
+
+        if (!Raycasts.RaycastNonAllocSorted(ray, false, false, layerMask))
+            return;
+
+        RaycastHit hit = Raycasts.closestHit;
+        Vector3 normal = hit.normal;
+        origin = hit.point;
+
+        //position
+        spawnObjs[i].transform.position = origin + offset;
+
+        // rotation
+        rotation = Quaternion.Euler(new Vector3(Random.Range(-5, 5), Random.Range(0, 360), Random.Range(-5, 5)));
+        spawnObjs[i].transform.rotation = rotation;
+
+        // scale
+        Vector3 lossyScale = new Vector3(Random.Range(0.5f, 1.5f), Random.Range(0.5f, 1.5f), Random.Range(0.5f, 1.5f));
+        
+        spawnObjs[i].transform.localScale = lossyScale;
+        
+
+
+        Physics.autoSimulation = true;
+    }
+
+    private void RemoveObject(int i)
+    {
+        if (Application.isPlaying)
+            Destroy(spawnObjs[i]);
+        else
+            DestroyImmediate(spawnObjs[i]);
+
+        spawnObjs.ToList().RemoveAt(i);
+    }
+
+    private void UpdateObjects()
+    {
+        if (!Application.isPlaying && updateSettings)
+        {
+            InitObjects();
+            updateSettings = false;
+        }
+
+        for (int i = 0; i < spawnObjs.Length; i++)
+        {
+            if (spawnObjs[i] == null) return;
+
+            float distance = (new Vector2(spawnObjs[i].transform.position.x, spawnObjs[i].transform.position.z) - new Vector2(player.transform.position.x, player.transform.position.z)).sqrMagnitude;
+
+            if (distance > spawnSize * spawnSize)
+            {
+                spawnObjs[i].SetActive(false);
+
+                Spawn(i, DrawCircle(player.transform.position, spawnSize * 0.85f));
+            }
+            else
+                spawnObjs[i].SetActive(true);
+        }
+    }
+
+    private Vector3 DrawCircle(Vector3 center, float radius)
+    {
+        float ang = Random.value * 360;
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y;
+        pos.z = center.z + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        return pos;
+    }
+}
